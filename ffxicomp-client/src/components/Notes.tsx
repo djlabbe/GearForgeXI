cea29ff7223a6ad659f867a13cfacec68477f6e0
Doug Labbe <dougjlabbe@gmail.com>
2025-07-31 11:46:12 -0700

    list cleanup

--- ffxicomp-client/src/components/Notes.tsx
+++ ffxicomp-client/src/components/Notes.tsx
@@ -2,9 +2,9 @@ import Card from "./Card";
 
 const Notes = () => {
     return (
-        <Card>
-            <h2 className="font-semibold text-lg mb-2">Notes & Disclaimers</h2>
-            <ul className="list-disc list-inside space-y-2">
+        <Card className="my-4">
+             <h3 className="font-semibold mb-2">Notes and Disclaimers</h3>
+            <ul className="text-sm list-disc list-inside space-y-2">
                 <li>Stats on items may be missing or incorrect.</li>
                 <li>Many items are missing. This is very much a work in progress. New items will continue to be added. (Hopefully in bulk).</li>
                 <li>Items are missing certain stats such as defense, HP, and MP. I hope to eventually fill in these gaps.</li>
