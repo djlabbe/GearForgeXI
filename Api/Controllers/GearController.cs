3b41d3c0865cc6a3e8093bbd831dded69bebb0e6
Doug Labbe <dougjlabbe@gmail.com>
2025-08-08 16:44:47 -0700

    Gear order

--- Api/Controllers/GearController.cs
+++ Api/Controllers/GearController.cs
@@ -83,6 +83,8 @@ public class GearController(GearDbContext context) : ControllerBase
                  g.GearItemJobs.Count == 0) &&
                 g.GearItemSlots.Any(s => s.GearSlot.Name.ToLower() == slotNormalized))
             .OrderBy(g => g.Name)
+                .ThenBy(g => g.Path)
+                    .ThenBy(g => g.Rank)
             .Select(g => new GearItemDto
             {
                 Id = g.Id,
